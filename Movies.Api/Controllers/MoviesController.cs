d427e29a481e4692769ba32d11a99e83e1e9e55b
Amos Alfasi <user@DESKTOP-4BVA80H>
2025-07-19 10:53:14 +0300

    CreatedAtAction - idOrSlug

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -24,8 +24,8 @@ namespace Movies.Api.Controllers
         {
             var movie = request.MapToMovie();
             await _movieRepository.CreateAsync(movie);
-            return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
-            // return Created($"/api/movies/{movie.Id}",movie);
+            return CreatedAtAction(nameof(Get), new { idOrSlug = movie.Id }, movie);
+           // return Created($"/api/movies/{movie.Id}",movie);
         }
 
         [HttpGet(ApiEndpoints.Movies.Get)]
