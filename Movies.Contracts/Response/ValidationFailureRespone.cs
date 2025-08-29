704e26335c7a8c779af459c2b9f2201bfdafcc9a
Amos Alfasi <user@DESKTOP-4BVA80H>
2025-08-29 12:44:13 +0300

    Adding Validation

---
+++ Movies.Contracts/Response/ValidationFailureRespone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Response
+{
+    public class ValidationFailureRespone
+    {
+        public required IEnumerable<ValidationRespone> Errors { get; set; }
+    }
+
+    public class ValidationRespone
+    {
+        public required string PropertyName { get; init; }
+        public required string Message { get; init; }
+    }
+}
